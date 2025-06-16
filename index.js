70188a2a389de4d43d00c593c656e86ce0758fee
Gabriel Cabrejas <gabriel@cabrejas.net>
2025-06-15 20:40:52 -0700

    starter

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-const server = require('./api/server')
-
-const PORT = process.env.PORT || 3000
-
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`)
-})
+// entry point of the program
